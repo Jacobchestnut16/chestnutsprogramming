e4bf590f706e7c7c82bbff89020011b0cd4b375a
Jacobchestnut16 <88856888+Jacobchestnut16@users.noreply.github.com>
2023-04-23 15:08:53 -0400

    1.95 public

--- chestnutsProgrammingScript.js
+++ chestnutsProgrammingScript.js
@@ -16,3 +16,12 @@ function sendEmail() {
             alert("mail sent successfully")
         });
 }
+
+function iframeLoaded() {
+    var iFrameID = document.getElementById('page');
+    if (iFrameID) {
+        // here you can make the height, I delete it first, then I make it again
+        iFrameID.height = "";
+        iFrameID.height = iFrameID.contentWindow.document.body.scrollHeight + 150 + "px";
+    }
+}
